440f4555826f1c0ff197913b1ab102414e68324b
fal-mathPC <takada.y.ak@m.titech.ac.jp>
2024-10-18 22:53:42 +0900

    fix

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 const video = document.getElementById("camera");
-const whitelist = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわゐゑをん';
+const hiragana = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわゐゑをん';
 
 // カメラ映像を取得して表示
 navigator.mediaDevices.getUserMedia({ video: true })
@@ -19,27 +19,39 @@ document.getElementById("capture").addEventListener("click", () => {
   context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
   // Tesseract.jsを使ってOCRを実行
-  Tesseract.recognize(
-    canvas,
-    'jpn',
-    whitelist,
-    {
-      logger: (m) => console.log(m)
-    }
-  ).then(({ data: { text } }) => {
-    document.getElementById("result").innerText = `判別結果: ${text}`;
+  const text = recognize(canvas);
 
-    // 50枚の札に含まれるかを判別
-    const storedCards = localStorage.getItem("karutaCards");
-    const karutaCards = storedCards ? JSON.parse(storedCards) : [];
+  document.getElementById("result").innerText = `判別結果: ${text}`;
 
-    if (karutaCards.includes(text.trim())) {
-      document.getElementById("result").innerText += "（試合で使用します）";
-    } else {
-      document.getElementById("result").innerText += "（試合で使用しません）";
-    }
-  });
+  // 50枚の札に含まれるかを判別
+  const storedCards = localStorage.getItem("karutaCards");
+  const karutaCards = storedCards ? JSON.parse(storedCards) : [];
+
+  if (karutaCards.includes(text.trim())) {
+    document.getElementById("result").innerText += "（試合で使用します）";
+  } else {
+    document.getElementById("result").innerText += "（試合で使用しません）";
+  }
+  ;
 });
+
+async function recognize(canvas, lang = 'jpn', whitelist = hiragana) {
+  const worker = createWorker({
+    langPath: '...',
+    logger: m => console.log(m),
+  });
+  await worker.load();
+  // 言語を設定
+  await worker.loadLanguage(lang);
+  await worker.initialize(lang);
+  // 限定する文字を設定
+  await worker.setParameters({
+    tessedit_char_whitelist: whitelist
+  });
+  // 認識
+  const { data: { text } } = await worker.recognize(canvas);
+  return text;
+}
 
 // ローカルストレージに50枚の札を保存する例
 function saveKarutaCards(cards) {
